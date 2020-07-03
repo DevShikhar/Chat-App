1bfada18350e222b0d5f8cbd349300ddb174f4ea
DevShikhar <shikhar1481997@gmail.com>
2020-07-03 19:58:04 +0530

    added connect/disconnect

--- server/server.js
+++ server/server.js
@@ -1,16 +1,31 @@
 const   express = require('express'),
-        path = require('path');
+        path = require('path'),
+        http = require('http'),
+        socketIO = require('socket.io');
+const { Socket } = require('net');
 
 
 
 const app = express();
 const publicPath = path.join(__dirname, '../public');
+const server = http.createServer(app);
+const io = socketIO(server);
 
 require('dotenv').config();
 app.use(express.static(publicPath));
 
+io.on("connection", (socket)=>{
+    console.log(`NEW USER CONNECTED`);  
 
-app.listen(process.env.PORT, ()=>{
+    socket.on("disconnect", ()=>{
+        console.log(`USER DISCONNECTED`);
+    })
+});
+
+
+
+
+server.listen(process.env.PORT, ()=>{
     console.log(`SERVER STARTED AT ${process.env.PORT}`);
 })
 
